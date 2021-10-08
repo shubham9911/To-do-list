e3af9415e8071798890676a79be83d0b13052a8a
shubham9911 <shubh9911am@gmail.com>
2021-10-08 11:52:37 +0530

    Updates

---
+++ precache-manifest.6fa01709b852db58844fc214002669e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0793ad6c4a0f18315773b6cdb6c2b7",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "2a0dc534756f00a8cfe1",
+    "url": "/To-do-list/static/css/main.e75ba4c9.chunk.css"
+  },
+  {
+    "revision": "e7266630377b76546664",
+    "url": "/To-do-list/static/js/2.a592b1cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list/static/js/2.a592b1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0dc534756f00a8cfe1",
+    "url": "/To-do-list/static/js/main.f8d419f9.chunk.js"
+  },
+  {
+    "revision": "d9924bae88e1079551da",
+    "url": "/To-do-list/static/js/runtime-main.17841008.js"
+  }
+]);
